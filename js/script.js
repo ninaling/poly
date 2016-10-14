0f99f27c3d3fcb4716d0bcb7cac4cf71289c63c5
thetoothless <ninaling@ucla.edu>
2016-10-14 02:20:04 -0700

    added some more examples

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var tmax_options = {
-  delay: 0.25, // init pause time
+  delay: 0.10, // init pause time
   repeat: -1, // make it repeat
-  repeatDelay: 0.25, // delay this amount on repeat
+  repeatDelay: 0.15, // delay this amount on repeat
   yoyo: true // also play sequence in reverse
 };
 
 var svg_length  = $('svg.blue-quartz > polygon').length,
     svg_shapes  = [],
     stagger_val = 0.00475,
-    duration    = 2.5;
+    duration    = 2.0;
 
 var tmax_tl = new TimelineMax(tmax_options);
 
@@ -36,3 +36,28 @@ var stagger_opts_to = {
 };
 
 tmax_tl.staggerFromTo(svg_shapes, duration, stagger_opts_from, stagger_opts_to, stagger_val, 0);
+
+$(document).ready(function(){
+  var ascensor = $('#ascensor').ascensor({
+    childType: "section",
+    ascensorFloorName: ['crystal','head', 'banana'],
+    time: 650,
+    windowsOn: 0,
+    direction: 'x',
+    easing: "easeInOutQuart"
+  });
+
+  var ascensorInstance = ascensor.data('ascensor');
+
+  var head = new Vivus('head-img', {type: 'delayed', duration: 220, delay: 200, start: 'autostart'});
+
+  var graphics = [head];
+
+  ascensor.on("scrollStart", function(e, floor){
+    head.stop().play(-5);
+  });
+
+  ascensor.on("scrollEnd", function(e, floor){
+    head.play();
+  });
+});
